6f9e89761770e7e0678a4e0b38e464603db075f6
IvanAbadjiev <ivan087611@gmail.com>
2022-11-05 23:36:33 +0200

    Add new task

---
+++ First Steps In Coding/Lab/T04.InchesToCentimeters/T04.InchesToCentimeters/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace T04.InchesToCentimeters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cm = double.Parse(Console.ReadLine());
+            double inches = 2.54;
+
+            double convertor = cm * inches;
+
+            Console.WriteLine(convertor);
+
+        }
+    }
+}
