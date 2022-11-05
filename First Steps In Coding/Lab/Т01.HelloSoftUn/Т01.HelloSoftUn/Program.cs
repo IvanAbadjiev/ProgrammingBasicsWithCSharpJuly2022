94d96c1a64f0e3116ecb42839c5b8fb372feb9cb
IvanAbadjiev <ivan087611@gmail.com>
2022-11-05 23:35:36 +0200

    Add new task

---
+++ First Steps In Coding/Lab/Т01.HelloSoftUn/Т01.HelloSoftUn/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Т01.HelloSoftUn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SoftUni");
+        }
+    }
+}
