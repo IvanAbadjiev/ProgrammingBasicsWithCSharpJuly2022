0e4077d4b5b6c55267248582e74a0311874e8da4
IvanAbadjiev <ivan087611@gmail.com>
2022-11-05 23:37:52 +0200

    Add new task

---
+++ First Steps In Coding/Lab/T07.ProjectsCreation/T07.ProjectsCreation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace T07.ProjectsCreation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string аrchitectName = Console.ReadLine();
+            int howMenyProject = int.Parse(Console.ReadLine());
+            int hourNead = howMenyProject * 3;
+
+            Console.WriteLine($"The architect {аrchitectName} will need {hourNead} hours to complete {howMenyProject} project/s.");
+        }
+    }
+}
---
+++ First Steps In Coding/Lab/T08.PetShop/T08.PetShop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace T08.PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogFood = int.Parse(Console.ReadLine());
+            int catFood = int.Parse(Console.ReadLine());
+
+            double dogPrice = dogFood * 2.50;
+            double catPrice = catFood * 4;
+
+            double sum = catPrice + dogPrice;
+
+            Console.WriteLine($"{sum} lv.");
+        }
+    }
+}
