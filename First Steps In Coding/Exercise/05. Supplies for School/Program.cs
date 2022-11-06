1951bd1b73a6337ba08e861d0baee7e0d309ae0e
IvanAbadjiev <ivan087611@gmail.com>
2022-11-06 23:28:32 +0200

    Add new task

---
+++ First Steps In Coding/Exercise/05. Supplies for School/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _05._Supplies_for_School
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+           int numberPen = int.Parse(Console.ReadLine());
+            int numberMarker = int.Parse(Console.ReadLine());
+            int preparation = int.Parse(Console.ReadLine());
+
+            int discount = int.Parse(Console.ReadLine());
+
+            double penPrice = numberPen * 5.80;
+            double markerPrice = numberMarker * 7.20;
+            double preparationPrice = preparation * 1.20;
+
+
+            double ds =  discount / 100.00;
+
+
+            double sum = penPrice + markerPrice + preparationPrice;
+
+            double priceWithDiscount = sum - (sum * ds);
+
+           
+
+            Console.WriteLine(priceWithDiscount);
+        }
+    }
+}
---
+++ First Steps In Coding/Exercise/06. Repainting/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _06._Repainting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nylon = int.Parse(Console.ReadLine());
+            int peint = int.Parse(Console.ReadLine());
+            int razreditel = int.Parse(Console.ReadLine());
+            int hour = int.Parse(Console.ReadLine());
+
+            double peintProcent = peint * 0.10;
+
+
+            double nylonPrice = (nylon + 2) * 1.50;
+            double peintPrice = (peint + peintProcent) * 14.50;
+            double razreditelPrice = razreditel * 5.00;
+            double bagPrice = 0.40;
+
+            double sumMaterial = nylonPrice + peintPrice + razreditelPrice + bagPrice;
+
+
+            double procentOfSum = sumMaterial / 100 * 30 * hour;
+
+            double print = sumMaterial + procentOfSum;
+
+            Console.WriteLine(print);
+
+
+        }
+    }
+}
