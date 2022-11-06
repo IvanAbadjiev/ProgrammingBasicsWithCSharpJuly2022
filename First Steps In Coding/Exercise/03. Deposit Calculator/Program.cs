afbd8ea41ab6e04d31f341d3cd2a96c4fd5595b9
IvanAbadjiev <ivan087611@gmail.com>
2022-11-06 23:27:57 +0200

    Add new task

---
+++ First Steps In Coding/Exercise/03. Deposit Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03._Deposit_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double depositMoney = double.Parse(Console.ReadLine());
+            int time = int.Parse(Console.ReadLine());
+            double year = double.Parse(Console.ReadLine());
+
+            double procent = year / 100;
+
+            double sum = depositMoney + time * ((depositMoney * procent) / 12);
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ First Steps In Coding/Exercise/04. Vacation Books List/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04._Vacation_Books_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int listNumberInBook = int.Parse(Console.ReadLine());
+            int listReadPerHour = int.Parse(Console.ReadLine());
+            int dayReadBook = int.Parse(Console.ReadLine());
+
+            int calculate = listNumberInBook / listReadPerHour;
+
+            int day = calculate / dayReadBook;
+
+            Console.WriteLine(day);
+
+        }
+    }
+}
