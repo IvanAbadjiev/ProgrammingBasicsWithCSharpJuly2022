744bc282174b3cd70037cc8458ddf81871fa3612
IvanAbadjiev <ivan087611@gmail.com>
2022-11-09 22:12:11 +0200

    Add new task

---
+++ Conditional Statements/Lab/01. Excellent Result/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Xml;
+
+namespace _01._Excellent_Result
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double incom = double.Parse(Console.ReadLine());
+
+
+            if (incom >= 5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+
+            }
+        }
+    }
+
