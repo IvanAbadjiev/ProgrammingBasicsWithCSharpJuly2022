eadb635ba02a16fe1da9860ac68e6692f8d036d9
IvanAbadjiev <ivan087611@gmail.com>
2022-11-09 22:15:40 +0200

    Add new task

---
+++ Conditional Statements/Lab/06. Speed Info/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _06._Speed_Info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double speed = double.Parse(Console.ReadLine());
+
+            String print = "";
+            
+            if (speed <= 10)
+            {
+                print = "slow";
+            } else if (speed <= 50)
+            {
+                print = "average";
+            } else if (speed <= 150)
+            {
+                print = "fast";
+            } else if (speed <= 1000)
+            {
+                print = "ultra fast";
+            }
+            else
+            {
+                print = "extremely fast";
+            }
+
+
+            Console.WriteLine(print);
+        }
+
+    }
+}
---
+++ Conditional Statements/Lab/07. Area of Figures/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _07._Area_of_Figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+
+            double print = 0;
+
+
+            if(figure == "square")
+            {
+                double side = double.Parse(Console.ReadLine());
+                print = side * side;
+
+
+            } else if (figure == "rectangle")
+            {
+                double side1 = double.Parse(Console.ReadLine());
+                double side2 = double.Parse(Console.ReadLine());
+
+                print = side1 * side2;
+
+            } else if (figure == "circle")
+            {
+                double radios = double.Parse(Console.ReadLine());
+                print = Math.PI * (Math.Pow(radios, 2));
+
+
+            } else if (figure == "triangle")
+            {
+                double side1 = double.Parse(Console.ReadLine());
+                double side2 = double.Parse(Console.ReadLine());
+
+                print = side1 * side2 / 2;
+
+            }
+            Console.WriteLine(print);
+        }
+    }
+}
