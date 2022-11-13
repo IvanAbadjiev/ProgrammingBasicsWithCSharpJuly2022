2ea53ea797c7635c0ebbf89683af6cdde4523af9
IvanAbadjiev <ivan087611@gmail.com>
2022-11-13 21:53:03 +0200

    Add new task

---
+++ Conditional Statements/Exercise/04. Toy Shop/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _04._Toy_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double needMoney = double.Parse(Console.ReadLine());
+            int puzel = int.Parse(Console.ReadLine());
+            int dog = int.Parse(Console.ReadLine());
+            int bear = int.Parse(Console.ReadLine());
+            int minion = int.Parse(Console.ReadLine());
+            int truck = int.Parse(Console.ReadLine());
+
+            double sumPrice =
+                puzel * 2.60 +
+                dog * 3 +
+                bear * 4.10 +
+                minion * 8.20 +
+                truck * 2;
+
+            int sellToys = puzel + dog + bear + minion + truck;
+
+            if (sellToys >= 50)
+            {
+                sumPrice *= 0.75;
+            }
+
+            sumPrice *= 0.90;
+
+            double lessMoney = sumPrice - needMoney;
+
+            if (sumPrice >= needMoney)
+            {
+                Console.WriteLine($"Yes! {lessMoney:F2} lv left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! {Math.Abs(lessMoney):F2} lv needed.");
+            }
+           
+        }
+    }
+}
---
+++ Conditional Statements/Exercise/05. Godzilla vs. Kong/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _05._Godzilla_vs._Kong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int statist = int.Parse(Console.ReadLine());
+            double StaffForOneStatistPrice = double.Parse(Console.ReadLine());
+
+
+            double decorPrice = budget * 0.10;
+            double staffPrice = StaffForOneStatistPrice * statist;
+
+            if (statist > 150)
+            {
+                staffPrice *= 0.90;
+            }
+
+            double needMoney = decorPrice + staffPrice;
+            double difference = budget - needMoney;
+
+            if (needMoney > budget)
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {Math.Abs(difference):F2} leva more.");
+            }
+            else
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {difference:F2} leva left.");
+            }
+
+
+
+
+           
+        }
+    }
+}
