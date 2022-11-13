f065fa1823a0b7af2b4763312eb4a85c0bde0435
IvanAbadjiev <ivan087611@gmail.com>
2022-11-13 21:53:42 +0200

    Add new task

---
+++ Conditional Statements/Exercise/06. World Swimming Record/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _06._World_Swimming_Record
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double recordInSecond = double.Parse(Console.ReadLine());
+            double metars = double.Parse(Console.ReadLine());
+            double timePerKilometer = double.Parse(Console.ReadLine());
+
+            double time = timePerKilometer * metars;
+            double waterProof = Math.Floor(metars / 15) * 12.5;
+
+            double fullTime = time + waterProof;
+            double difference = fullTime - recordInSecond;
+            if (fullTime >= recordInSecond)
+            {
+                Console.WriteLine($"No, he failed! He was {Math.Abs(difference):F2} seconds slower.");
+            }
+            else if (fullTime < recordInSecond)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {fullTime:F2} seconds.");
+            }
+
+          
+          
+        }
+    }
+}
---
+++ Conditional Statements/Exercise/07. Shopping/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _07._Shopping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double petarBudget = double.Parse(Console.ReadLine());
+            int card = int.Parse(Console.ReadLine());
+            int procesor = int.Parse(Console.ReadLine());
+            int ram = int.Parse(Console.ReadLine());
+
+            double cardPrice = 250 * card;
+            double procesorPrice = cardPrice *  0.35 * procesor;
+            double ramPrice = cardPrice * 0.10 * ram;
+
+            double totalSum = cardPrice + procesorPrice + ramPrice;
+
+            if (card  > procesor)
+            {
+                totalSum *= 0.85;
+            }
+
+            double difference = petarBudget - totalSum;
+
+            if (petarBudget >= totalSum)
+            {
+                Console.WriteLine($"You have {difference:F2} leva left!");
+            } else
+            {
+                Console.WriteLine($"Not enough money! You need {Math.Abs(difference):F2} leva more!");
+            }
+
+            Console.WriteLine();
+    
+
+        }
+    }
+}
