cd3274e77c7674581f4957338f9936b4029c5da3
IvanAbadjiev <ivan087611@gmail.com>
2023-01-02 13:15:41 +0200

    Add new task

---
+++ Conditional Statements Advanced/Lab/08.Cinema Ticket/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _08.Cinema_Ticket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dayOfWeek = Console.ReadLine();
+
+            int priceTicet = 0;
+            switch (dayOfWeek)
+            {
+                case "Monday":
+                    priceTicet = 12;
+                    break;
+                case "Tuesday":
+                    priceTicet = 12;
+                    break;
+                case "Wednesday":
+                    priceTicet = 14;
+                    break;
+                case "Thursday":
+                    priceTicet = 14;
+                    break;
+                case "Friday":
+                    priceTicet = 12;
+                    break;
+                case "Saturday":
+                case "Sunday":
+                    priceTicet = 16;
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine(priceTicet);
+        }
+    }
+}
