62753f99c3fd7aa4b5d011aa64a2f4c61a9c55fa
IvanAbadjiev <ivan087611@gmail.com>
2023-01-03 16:48:47 +0200

    Add new task

---
+++ Conditional Statements Advanced/Exercise/01. Cinema/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01._Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            int r = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+                int full = r * c;
+
+            switch (type)
+            {
+                case "Premiere":
+                    Console.WriteLine($"{(full * 12):F2} leva");
+                    break;
+                case "Normal":
+                    Console.WriteLine($"{(full * 7.50):F2} leva");
+                    break;
+                case "Discount":
+                    Console.WriteLine($"{(full * 5):F2} leva");
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
