86a606828c0a4811a913b8456f6808cf99bf8298
IvanAbadjiev <ivan087611@gmail.com>
2023-01-05 21:50:46 +0200

    In this lesson we will learn
    • For-loop;
    • Control variables;
    • Increment.

---
+++ For Loop/Lab/08. Number sequence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _08._Number_sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int maxNumber = int.MinValue;
+            int minNumber = int.MaxValue;
+
+
+            for (int i = 0; i < n; i++)
+            {
+                int curentNumber = int.Parse(Console.ReadLine());
+
+                if (curentNumber > maxNumber)
+                {
+                    maxNumber = curentNumber;
+                }
+                 if (curentNumber < minNumber)
+                {
+                    minNumber = curentNumber;
+                }
+
+            }
+            Console.WriteLine($"Max number: {maxNumber}");
+            Console.WriteLine($"Min number: {minNumber}");
+        }
+    }
+}
