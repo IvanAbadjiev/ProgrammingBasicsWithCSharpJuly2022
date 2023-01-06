3e6b1a6c140b1c7c5f0d6439991ad2a865cc1c53
IvanAbadjiev <ivan087611@gmail.com>
2023-01-06 09:46:32 +0200

    In this lesson we will learn
    • For-loop;
    • Control variables;
    • Increment.

---
+++ For Loop/Lab/09. Left and Right Sum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _09._Left_and_Right_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int sum2 = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int num1 = int.Parse(Console.ReadLine());
+                sum += num1;   
+            }
+            for (int i = 0; i < n; i++)
+            {
+                int num1 = int.Parse(Console.ReadLine());
+                sum2 += num1;
+            }
+            int diff = Math.Abs(sum - sum2);
+
+            if (sum == sum2)
+            {
+                Console.WriteLine($"Yes, sum = {sum}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {diff}");
+            }
+            
+
+        }
+    }
+}
