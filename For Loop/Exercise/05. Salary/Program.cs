d00a5bed659b9a16e8f35728df0ed5ec7500cb59
IvanAbadjiev <ivan087611@gmail.com>
2023-01-07 10:02:55 +0200

    In this lesson we will learn
    • For-loop;
    • Control variables;
    • Increment.

---
+++ For Loop/Exercise/05. Salary/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _05._Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tabs = int.Parse(Console.ReadLine());
+            int selary = int.Parse(Console.ReadLine());
+
+            bool printSelaryOrNotPrintSelary = true;
+
+            for (int i = 0; i < tabs; i++)
+            {
+                string siteInCurentTabs = Console.ReadLine();
+
+
+
+                switch (siteInCurentTabs)
+                {
+                    case "Facebook":
+                        selary -= 150;
+                        break;
+                    case "Instagram":
+                        selary -= 100;
+                        break;
+                    case "Reddit":
+                        selary -= 50;
+                        break;
+                    default:
+                        break;
+                }
+                if (selary <= 0)
+                {
+                    Console.WriteLine("You have lost your salary.");
+                    printSelaryOrNotPrintSelary = false;
+                    break;
+                }
+            }
+
+            if (printSelaryOrNotPrintSelary)
+            {
+            Console.WriteLine(selary);
+
+            }
+        }
+    }
+}
