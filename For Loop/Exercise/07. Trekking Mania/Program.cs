0e689e2c357da2742017053ce8f1df4dbd0bfcad
IvanAbadjiev <ivan087611@gmail.com>
2023-01-07 10:05:12 +0200

    In this lesson we will learn
    • For-loop;
    • Control variables;
    • Increment.

---
+++ For Loop/Exercise/07. Trekking Mania/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _07._Trekking_Mania
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int qtyGroup = int.Parse(Console.ReadLine());
+
+            int sumPeopleInAllGroup = 0;
+            int musala = 0;
+            int monblan = 0;
+            int kilimandjaro = 0;
+            int k2 = 0;
+            int everest = 0;
+
+            for (int i = 0; i < qtyGroup; i++)
+            {
+                int peopoleInOneGroup = int.Parse(Console.ReadLine());
+                sumPeopleInAllGroup += peopoleInOneGroup;
+
+                if (peopoleInOneGroup <= 5)
+                {
+                    musala += peopoleInOneGroup;
+                }
+                else if (peopoleInOneGroup <= 12)
+                {
+                    monblan += peopoleInOneGroup;
+                }
+                else if (peopoleInOneGroup <= 25)
+                {
+                    kilimandjaro += peopoleInOneGroup;
+                }
+                else if (peopoleInOneGroup <= 40)
+                {
+                    k2 += peopoleInOneGroup;
+                }
+                else
+                {
+                    everest += peopoleInOneGroup;
+                }
+
+            }
+
+            Console.WriteLine($"{(musala + 0.00) / sumPeopleInAllGroup * 100:f2}%");
+            Console.WriteLine($"{(monblan + 0.00) / sumPeopleInAllGroup * 100:f2}%");
+            Console.WriteLine($"{(kilimandjaro + 0.00) / sumPeopleInAllGroup * 100:f2}%");
+            Console.WriteLine($"{(k2 + 0.00) / sumPeopleInAllGroup * 100:f2}%");
+            Console.WriteLine($"{(everest + 0.00) / sumPeopleInAllGroup * 100:f2}%");
+        }
+    }
+}
