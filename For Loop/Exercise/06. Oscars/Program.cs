fd0ddbb76f6068e2d228da66f6b12515ded5555d
IvanAbadjiev <ivan087611@gmail.com>
2023-01-07 10:04:42 +0200

    In this lesson we will learn
    • For-loop;
    • Control variables;
    • Increment.

---
+++ For Loop/Exercise/06. Oscars/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _06._Oscars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string actiorName = Console.ReadLine();
+            double academyPoints = double.Parse(Console.ReadLine());
+            int elevationPeople = int.Parse(Console.ReadLine());
+
+            bool congratilation = false;
+
+            for (int i = 0; i < elevationPeople; i++)
+            {
+                string elevationName = Console.ReadLine();
+                double curentPoint = double.Parse(Console.ReadLine());
+
+                double sum = (elevationName.Length * curentPoint) / 2;
+                academyPoints += sum;
+                if (academyPoints > 1250.5)
+                {
+                    congratilation = true;
+                    break;
+                }
+
+            }
+
+            double diff = Math.Abs(academyPoints - 1250.5);
+            if (congratilation)
+            {
+                Console.WriteLine($"Congratulations, {actiorName} got a nominee for leading role with {academyPoints:f1}!"
+);
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, {actiorName} you need {diff:f1} more!"
+);
+            }
+        }
+    }
+}
