9ab3119eeddbf72c4dc7277f1d569b7ea3ed041c
IvanAbadjiev <ivan087611@gmail.com>
2023-01-10 23:17:30 +0200

    Exercise

---
+++ While Loop/Exercise/04. Walking/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _04._Walking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+
+            int sumStep = 0;
+            while (input != "Going home")
+            {
+                input = Console.ReadLine();
+                if (input == "Going home")
+                {
+                    break;
+                }
+                sumStep += int.Parse(input);
+
+                if (sumStep >= 10000)
+                {
+                    Console.WriteLine("Goal reached! Good job!");
+                    Console.WriteLine($"{sumStep - 10000} steps over the goal!");
+                    break;
+                } 
+
+            }
+            if (input == "Going home")
+            {
+                input = Console.ReadLine();
+                sumStep += int.Parse(input);
+                if (sumStep >= 10000)
+                {
+                    Console.WriteLine("Goal reached! Good job!");
+                    Console.WriteLine($"{Math.Abs(sumStep - 10000)} steps over the goal!");
+                    
+                }
+                else
+                {
+                    Console.WriteLine($"{Math.Abs(sumStep - 10000)} more steps to reach goal.");
+                }
+            }
+        }
+    }
+}
