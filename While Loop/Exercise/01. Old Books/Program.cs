76815249fc684ebb39c510fa9292b343e19944b7
IvanAbadjiev <ivan087611@gmail.com>
2023-01-10 23:15:31 +0200

    Exercise

---
+++ While Loop/Exercise/01. Old Books/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _01._Old_Books
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string shirchBookForAni = Console.ReadLine();
+
+            string aniBook = shirchBookForAni;
+
+            int counterBookChek = 0;
+
+            bool findAniBook = false;
+
+            while (shirchBookForAni != "No More Books")
+            {
+                shirchBookForAni = Console.ReadLine();
+
+                if (shirchBookForAni == "No More Books")
+                {
+                    break;
+                }
+                if (shirchBookForAni == aniBook)
+                {
+                    findAniBook = true;
+                    Console.WriteLine($"You checked {counterBookChek} books and found it.");
+                    break;
+                }
+                counterBookChek++;
+
+            }
+
+            if (!findAniBook)
+            {
+                Console.WriteLine($"The book you search is not here!");
+                Console.WriteLine($"You checked {counterBookChek} books.");
+
+
+            }
+        }
+    }
+}
