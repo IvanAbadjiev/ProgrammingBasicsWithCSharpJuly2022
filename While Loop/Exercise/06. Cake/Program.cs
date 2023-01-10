f6d71c995699bc2717ce3ee49d7e4d63aac7f6b0
IvanAbadjiev <ivan087611@gmail.com>
2023-01-10 23:19:00 +0200

    Exercise

---
+++ While Loop/Exercise/06. Cake/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _06._Cake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int shi = int.Parse(Console.ReadLine());
+           int dul = int.Parse(Console.ReadLine());
+
+            int pieces = shi * dul;
+            string curentPieces = Console.ReadLine();
+            bool isVaid = false;
+
+            while (curentPieces != "STOP")
+            {
+                pieces -= int.Parse(curentPieces);
+                if (pieces <= 0)
+                {
+                    isVaid = true;
+                    Console.WriteLine($"No more cake left! You need {Math.Abs(pieces)} pieces more.");
+                    break;
+                }
+
+
+
+                curentPieces = Console.ReadLine();
+            }
+            if (!isVaid)
+            {
+                Console.WriteLine($"{pieces} pieces are left.");
+
+            }
+        }
+    }
+}
