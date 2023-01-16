75ecd22e6b7a1ec662a79c4086cae1c27fad0709
IvanAbadjiev <ivan087611@gmail.com>
2023-01-16 22:50:11 +0200

    • Nested loops; • More complex tasks with repetition structures.

---
+++ Nested Loops/Lab/03. Combinations/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _03._Combinations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int counter = 0;
+            for (int one = 0; one <= n ; one++)
+            {
+                for (int two = 0; two <= n; two++)
+                {
+                    for (int third = 0; third <= n; third++)
+                    {
+                        if (one + two + third == n)
+                        {
+                            counter++;
+                        }
+                    }
+
+                }
+
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
