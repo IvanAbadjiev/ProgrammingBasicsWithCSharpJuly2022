05694c93bda65f57473c05310b1902bab2646727
IvanAbadjiev <ivan087611@gmail.com>
2023-01-16 22:44:17 +0200

    Exercise

---
+++ While Loop/Exercise/07. Moving/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _07._Moving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int shi = int.Parse(Console.ReadLine());  
+            int dul = int.Parse(Console.ReadLine());  
+            int vis = int.Parse(Console.ReadLine());
+
+            int freeCubicMeters = shi * dul * vis;
+            int sumBoxCubicMeters = 0;
+            bool isValid = false;
+
+            string box = Console.ReadLine();
+            while (box != "Done")
+            {
+                sumBoxCubicMeters += int.Parse(box);
+                if (sumBoxCubicMeters > freeCubicMeters)
+                {
+                    isValid = true;
+                    Console.WriteLine($"No more free space! You need {Math.Abs(freeCubicMeters - sumBoxCubicMeters)} Cubic meters more."
+    ); break;
+                }
+                box = Console.ReadLine();   
+            }
+            if (!isValid)
+            {
+                Console.WriteLine($"{freeCubicMeters - sumBoxCubicMeters} Cubic meters left.");
+
+            }
+            
+
+        }
+    }
+}
