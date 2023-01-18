32fbebfd8da430da477682d1cd074d97dad2e0ca
IvanAbadjiev <ivan087611@gmail.com>
2023-01-18 15:41:48 +0200

    • Nested loops; • More complex tasks with repetition structures.

---
+++ Nested Loops/Lab/05. Travelling/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _05._Travelling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+
+            while (destination != "End")
+            {
+                Double budget = double.Parse(Console.ReadLine());
+                double saveMoney = 0;
+                while (budget > saveMoney)
+                {
+                    saveMoney += double.Parse(Console.ReadLine());
+
+                }
+                Console.WriteLine($"Going to {destination}!");
+
+                destination = Console.ReadLine();
+            }
+
+        }
+    }
+}
