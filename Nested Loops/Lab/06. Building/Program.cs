898866c417e85e4e079bd59aa5be1a9c7d03bdfa
IvanAbadjiev <ivan087611@gmail.com>
2023-01-18 15:42:15 +0200

    • Nested loops; • More complex tasks with repetition structures.

---
+++ Nested Loops/Lab/06. Building/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _06._Building
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int flor = int.Parse(Console.ReadLine());
+            int apartament = int.Parse(Console.ReadLine());
+
+
+            for (int curentFlor = flor; curentFlor >= 1; curentFlor--)
+            {
+                for (int curentApart = 0; curentApart < apartament; curentApart++)
+                {
+                    if (curentFlor == flor)
+                    {
+                        Console.Write($"L{curentFlor}{curentApart} ");
+                    }
+                    else if (curentFlor % 2 == 0)
+                    {
+                        Console.Write($"O{curentFlor}{curentApart} ");
+                    }
+                    else if (curentFlor % 2 != 0)
+                    {
+                        Console.Write($"A{curentFlor}{curentApart} ");
+                    }
+
+
+                }
+                Console.WriteLine();
+
+
+            }
+
+        }
+    }
+}
