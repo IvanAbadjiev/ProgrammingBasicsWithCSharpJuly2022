b234c6cd21b4b3be51ef0f4167c621d4e0b54619
IvanAbadjiev <ivan087611@gmail.com>
2023-01-19 18:14:16 +0200

    • Nested loops; • More complex tasks with repetition structures.

---
+++ Nested Loops/Exercise/03. Sum Prime Non Prime/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _03._Sum_Prime_Non_Prime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int primeNum = 0;
+            int nonPrimeNum = 0;
+
+            
+            while (input != "stop")
+            {
+                int curentNum = int.Parse(input);
+                bool isNonPrimeNumber = true;
+                if (curentNum < 0 )
+                {
+                    Console.WriteLine("Number is negative.");
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                for (int i = 2; i < curentNum; i++)
+                {
+                    if (curentNum % i == 0)
+                    {
+                        primeNum += curentNum;
+                        isNonPrimeNumber = false;
+                        break;
+                    }
+                    
+                }
+
+                if (isNonPrimeNumber)
+                {
+                    nonPrimeNum += curentNum;
+                }
+
+                input = Console.ReadLine();
+
+            }
+
+            Console.WriteLine($"Sum of all prime numbers is: {nonPrimeNum}");
+            Console.WriteLine($"Sum of all non prime numbers is: {primeNum}");
+        }
+    }
+}
