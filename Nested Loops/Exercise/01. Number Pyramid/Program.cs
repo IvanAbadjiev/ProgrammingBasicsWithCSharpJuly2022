a9fc3558803af902b8bcff4ba819540f1919a724
IvanAbadjiev <ivan087611@gmail.com>
2023-01-19 18:13:29 +0200

    • Nested loops; • More complex tasks with repetition structures.

---
+++ Nested Loops/Exercise/01. Number Pyramid/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _01._Number_Pyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int curentNum = 0;
+            bool flag = false;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    curentNum++;
+                    Console.Write(curentNum + " ");
+                    if (curentNum >= n)
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+                    Console.WriteLine();
+                if (flag)
+                {
+                    break;
+                }
+            }
+
+
+        }
+    }
+}
