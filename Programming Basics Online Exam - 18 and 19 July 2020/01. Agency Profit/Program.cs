46df0d523aa654713b26fe60037f35b9935f2a1e
IvanAbadjiev <ivan087611@gmail.com>
2023-01-26 20:45:03 +0200

    Add task

---
+++ Programming Basics Online Exam - 18 and 19 July 2020/01. Agency Profit/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01._Agency_Profit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string agency = Console.ReadLine();
+            int ticket = int.Parse(Console.ReadLine());
+            int kidTicket = int.Parse(Console.ReadLine());
+            double netoPriceForTicket = double.Parse(Console.ReadLine());
+            double tax = double.Parse(Console.ReadLine());
+
+
+            double priceRegularTicket = (netoPriceForTicket + tax) * ticket;
+            double netPriceForKid = netoPriceForTicket - netoPriceForTicket * 0.70;
+            double priceKidTicket = (netPriceForKid + tax) * kidTicket;
+
+            double totalIncom = priceKidTicket + priceRegularTicket;
+
+            double netIncom = totalIncom - totalIncom * 0.80;
+
+            Console.WriteLine($"The profit of your agency from {agency} tickets is {netIncom:f2} lv.");
+
+        }
+    }
+}
