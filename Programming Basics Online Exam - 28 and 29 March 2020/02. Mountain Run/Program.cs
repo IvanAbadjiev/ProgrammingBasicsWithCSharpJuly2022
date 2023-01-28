d1f3910f2a83c502b1956a46cbc8db77acc01bc0
IvanAbadjiev <ivan087611@gmail.com>
2023-01-28 05:51:08 +0200

    Exam Exercises

---
+++ Programming Basics Online Exam - 28 and 29 March 2020/02. Mountain Run/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _02._Mountain_Run
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double recordInSeconds = double.Parse(Console.ReadLine());
+            double metars = double.Parse(Console.ReadLine());
+            double timeForPerMetars = double.Parse(Console.ReadLine());
+
+
+            double timePlus = (Math.Floor( metars / 50)) * 30;
+            double allTime = (metars * timeForPerMetars) + timePlus;
+
+
+            double diff = Math.Abs(allTime - recordInSeconds);
+
+            if (recordInSeconds <= allTime)
+            {
+                Console.WriteLine($"No! He was {diff:f2} seconds slower.");
+            }
+            else
+            {
+                Console.WriteLine($" Yes! The new record is {allTime:f2} seconds.");
+            }
+        }
+    }
+}
+
