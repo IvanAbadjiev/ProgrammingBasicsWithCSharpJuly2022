7accb8a5f6f6f0a4d4ce9c589d2a46faaf0db224
IvanAbadjiev <ivan087611@gmail.com>
2023-01-28 05:50:03 +0200

    Exam Exercises

---
+++ Programming Basics Online Exam - 28 and 29 March 2020/01. Change Bureau/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01._Change_Bureau
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int btcAlive = int.Parse(Console.ReadLine());
+            double cnyAlice = double.Parse(Console.ReadLine());
+            double changeComision = double.Parse(Console.ReadLine());
+
+            double btcChangeForBgn = btcAlive * 1168;
+            double cnyChangeForUsd = cnyAlice * 0.15;
+
+
+            double usdSwapbgn = cnyChangeForUsd * 1.76;
+            double bgnSwapEur = (usdSwapbgn + btcChangeForBgn) / 1.95;
+
+            double print =  bgnSwapEur - (bgnSwapEur / 100  * changeComision);
+
+            Console.WriteLine($"{print:f2}");
+            
+            
+        }
+    }
+}
