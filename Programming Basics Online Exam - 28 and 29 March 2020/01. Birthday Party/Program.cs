9a2d822c36fbbe15a18e418254f713351513da22
IvanAbadjiev <ivan087611@gmail.com>
2023-01-28 05:49:23 +0200

    Exam Exercises

---
+++ Programming Basics Online Exam - 28 and 29 March 2020/01. Birthday Party/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01._Birthday_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double rent = double.Parse(Console.ReadLine());
+
+            double cake = rent * 0.20;
+            double drink = cake - cake * 0.45;
+            double animation = rent / 3;
+
+            double moneyNeed = cake + drink + animation + rent;
+
+            Console.WriteLine(moneyNeed);
+
+        }
+    }
+}
