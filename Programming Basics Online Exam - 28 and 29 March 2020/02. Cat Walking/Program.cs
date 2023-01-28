0240a9b1d2e226b024b2583f0c4dcc15c0f9bba9
IvanAbadjiev <ivan087611@gmail.com>
2023-01-28 05:50:33 +0200

    Exam Exercises

---
+++ Programming Basics Online Exam - 28 and 29 March 2020/02. Cat Walking/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02._Cat_Walking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minWallkPearExit = int.Parse(Console.ReadLine());
+            int exit = int.Parse(Console.ReadLine());   
+            int calorieConsumation = int.Parse(Console.ReadLine());
+
+            int sumMinetWallk = minWallkPearExit * exit;
+            int burnCaloriesForDay = sumMinetWallk * 5;
+            int targetCalories = calorieConsumation / 2;
+
+            if (burnCaloriesForDay >= targetCalories)
+            {
+                Console.WriteLine($"Yes, the walk for your cat is enough. Burned calories per day: {burnCaloriesForDay}.");
+            } else
+            {
+                Console.WriteLine($"No, the walk for your cat is not enough. Burned calories per day: {burnCaloriesForDay}.");
+            }
+
+        }
+    }
+}
