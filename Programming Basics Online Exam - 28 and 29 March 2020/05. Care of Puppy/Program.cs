d5e7c4e2e87816418eae13aaf936a275950fdb62
IvanAbadjiev <ivan087611@gmail.com>
2023-01-29 18:06:58 +0200

    Exam Exercises

---
+++ Programming Basics Online Exam - 28 and 29 March 2020/05. Care of Puppy/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _05._Care_of_Puppy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int input = int.Parse(Console.ReadLine());
+            int foodAlive = input * 1000;
+            int eatFood = 0;
+
+            string curentEatFood = Console.ReadLine();
+            while (curentEatFood != "Adopted")
+            {
+                eatFood += int.Parse(curentEatFood);
+
+                curentEatFood = Console.ReadLine();
+            }
+
+
+            double diff = Math.Abs(foodAlive - eatFood);
+            if (eatFood > foodAlive)
+            {
+                Console.WriteLine($"Food is not enough. You need {diff} grams more.");
+            }
+            else
+            {
+                Console.WriteLine($"Food is enough! Leftovers: {diff} grams.");
+            }
+        }
+    }
+}
